18486f7feac09264c77c1f0f9cde4cd5ea85b4e7
Jonatas de Assis Silva <jonatasassis@MacBook-Pro-de-Jonatas.local>
2020-02-19 16:58:04 -0300

    Commit inicial

---
+++ pipefy/src/components/Card/index.js
@@ -0,0 +1,37 @@
+import React, { useRef } from 'react';
+import { useDrag, useDrop } from 'react-dnd';
+
+import { Container, Label } from './styles';
+
+export default function Card({ data }) {
+
+  const ref = useRef();
+
+  const [{ isDragging }, dragRef] = useDrag({
+    item: { type: 'CARD', id: data.id },
+    collect: monitor => ({
+      isDragging: monitor.isDragging(),
+    })
+  });
+
+  const [, dropRef] = useDrop({
+    accept: 'CARD',
+    hover(item, monitor) {
+      
+    }
+  })
+
+  dragRef(dropRef(ref));
+
+  return (
+    <Container ref={ref} isDragging={isDragging}>
+      <header>
+        {data.labels.map(label => <Label key={label} color={label} />)}
+      </header>
+
+      <p>{data.content}</p>
+
+      {data.user && <img src={data.user} alt="Avatar" />}
+    </Container>
+  );
+}
